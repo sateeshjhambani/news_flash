08d779e454dd68724e121ed35cf1d40fc029cd5b
Sateesh <sateesh.jh@gmail.com>
2023-03-28 20:29:50 +0400

    Created the constants file

---
+++ lib/utils/constants/constants.dart
@@ -0,0 +1,14 @@
+
+const int defaultPageSize = 20;
+
+// App
+const String appTitle = 'News Flash';
+
+// Networking and APIs
+const String baseUrl = 'https://newsapi.org/v2';
+const String defaultApiKey = '036469d81daa4365a787d919da4ae580'; // 'ff957763c54c44d8b00e5e082bc76cb0'
+const String defaultSources = 'bbc-news, abc-news, al-jazeera-english';
+
+// Storage and Databases
+const String articlesTableName = 'articles_table';
+const String databaseName = 'app_database.db';
