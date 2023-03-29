71b5f76e1cde1686cb1856e246f0c314105e1138
Sateesh <sateesh.jh@gmail.com>
2023-03-29 22:08:16 +0400

    Added the article details view

--- lib/presentation/views/breaking_news_view.dart
+++ lib/presentation/views/breaking_news_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:ionicons/ionicons.dart';
+import '../../config/router/app_router.dart';
 import '../../domain/model/article.dart';
 import '../../utils/extensions/scroll_controller_extensions.dart';
 import '../cubits/remote_articles/remote_articles_cubit.dart';
@@ -31,13 +32,13 @@ class BreakingNewsView extends HookWidget {
           style: TextStyle(color: Colors.black),
         ),
         actions: [
-          // GestureDetector(
-          //   onTap: () => appRouter.push(const SavedArticlesViewRoute()),
-          //   child: const Padding(
-          //     padding: EdgeInsets.symmetric(horizontal: 14),
-          //     child: Icon(Ionicons.bookmark, color: Colors.black),
-          //   ),
-          // ),
+          GestureDetector(
+            onTap: () => appRouter.push(const SavedArticlesViewRoute()),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 14),
+              child: Icon(Ionicons.bookmark, color: Colors.black),
+            ),
+          ),
         ],
       ),
       body: BlocBuilder<RemoteArticlesCubit, RemoteArticlesState>(
@@ -73,9 +74,9 @@ class BreakingNewsView extends HookWidget {
           delegate: SliverChildBuilderDelegate(
                 (context, index) => ArticleWidget(
               article: articles[index],
-              onArticlePressed: (e) => {} /*appRouter.push(
+              onArticlePressed: (e) => appRouter.push(
                 ArticleDetailsViewRoute(article: e),
-              )*/,
+              ),
             ),
             childCount: articles.length,
           ),
